42440d454b4bedf402ebabd5c6c9da2ed4c50a4f
meetadele <kovacsadel85@gmail.com>
2023-04-07 02:51:42 +0200

    drawing player cards

---
+++ README.md
@@ -0,0 +1,6 @@
+# Poker JavaScript game with API
+
+## Used tools:
+
+- https://deckofcardsapi.com/
+- `button-15` from https://getcssscan.com/css-buttons-examples page
--- poker.js
+++ poker.js
@@ -1,1 +1,41 @@
-alert("Hello, Javascript!");
+const newGameButton = document.querySelector(".js-new-game-button");
+const playerCardsContainer = document.querySelector(
+  ".js-player-cards-container"
+);
+
+playerCardsContainer.innerHTML = "here comes the cards";
+
+// program state
+let deckId = null;
+let playerCards = [];
+
+function renderPlayerCards() {
+  let html = "";
+
+  for (let card of playerCards) {
+    html += `<img src="${card.image}" alt="${card.code}" />`;
+  }
+
+  playerCardsContainer.innerHTML = html;
+}
+
+function drawAndRenderPlayerCards() {
+  if (deckId == null) return;
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then((data) => data.json())
+    .then(function (response) {
+      playerCards = response.cards;
+      renderPlayerCards();
+    });
+}
+
+function startGame() {
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then((data) => data.json())
+    .then(function (response) {
+      deckId = response.deck_id;
+      drawAndRenderPlayerCards(); // TODO: refactor with async-await
+    });
+}
+
+newGameButton.addEventListener("click", startGame);
